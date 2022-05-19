db073ffb0aba9e545f7c3cffe8bb5f9e03186298
smat67 <47300876+smat67@users.noreply.github.com>
2022-05-19 15:34:04 +0200

    task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,19 +1,21 @@
 #include "lists.h"
+
 /**
- * list_len - returns then number of elements in a list.
- * @h: singly linked list.
- * Return: number of elements in the list.
+ * list_len - prints length of list_t linked list
+ * @h: linked list
+ *
+ * Return: number of nodes (length) in linked list
  */
 
 size_t list_len(const list_t *h)
 {
-    size_t nelem;
+	size_t count = 0;
 
-    nelem = 0;
-    while (h != NULL)
-    {
-        h = h->next;
-        nelem++;
-    }
-    return (nelem);
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
 }
